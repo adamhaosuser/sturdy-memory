088c9381fb73b6c27633816464e596425b4be4c0
adamhaosuser <adammmeehan@gmail.com>
2025-08-21 13:26:38 +1000

    Update server.js
    Pivoting

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const app = express();
 app.use(express.json());
 
 app.post("/tickets", (req, res) => {
-  const { id, subject } = req.body;
-  console.log(`New Ticket: ID=${id}, Subject="${subject}"`);
+  const { id, assignee_id } = req.body;
+  console.log(`New Ticket: ID=${id}, Assignee ID="${assignee_id}"`);
   res.sendStatus(200);
 });
 
